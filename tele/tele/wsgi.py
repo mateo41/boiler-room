2b6186e6ce754ff8da11d7dda4fd1a3e2912bfca
Matt Rodriguez <mattrodriguez41@gmail.com>
2014-02-20 03:20:17 +0000

    Fixed the wsgi to use the libraries in a virtualenv.

--- tele/tele/wsgi.py
+++ tele/tele/wsgi.py
@@ -7,8 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
+import os,site
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tele.settings")
+
+site.addsitedir('/home/mateo/Python/boiler-room/tele')
+site.addsitedir('/home/mateo/.virtualenvs/boiler-room/lib/python2.7/site-packages')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
